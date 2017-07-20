8be8183c12d04d66a5b42fab23242df211d6cd79
Wei Lyu <lvwei1001@live.com>
2017-07-20 10:59:27 +0800

    add charset setting

--- mysql_to_excel.py
+++ mysql_to_excel.py
@@ -1,6 +1,7 @@
 import pymysql
 import json
 import openpyxl
+import codecs
 
 settings = json.loads(open('settings.json', encoding='utf-8').read())
 host = settings['host']
@@ -8,13 +9,18 @@ port = settings['port']
 user = settings['user']
 password = settings['password']
 database = settings['database']
-connection = pymysql.connect(host=host, port=port, user=user, password=password, database=database)
+charset = settings['charset']
+connection = pymysql.connect(host=host, port=port, user=user,
+                             password=password, database=database,
+                             charset=charset)
 tables = settings['tables']
 workbook = openpyxl.Workbook()
-with connection.cursor() as cursor:
-    for table in tables:
-        worksheet = workbook.create_sheet(table)
-        get_col_names_sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s"
+for table in tables:
+    worksheet = workbook.create_sheet(table)
+    with connection.cursor() as cursor:
+        get_col_names_sql = "SELECT COLUMN_NAME " + \
+            "FROM INFORMATION_SCHEMA.COLUMNS " + \
+            "WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s"
         count_col = cursor.execute(get_col_names_sql, (database, table))
         col_names = cursor.fetchall()
         get_data_sql = 'select * from ' + table
@@ -22,8 +28,7 @@ with connection.cursor() as cursor:
         data = cursor.fetchall()
         for i in range(len(col_names)):
             worksheet.cell(row=1, column=i + 1).value = col_names[i][0]
-        for row in range(len(data)):
-            for col in range(count_col):
-                worksheet.cell(row=row + 2, column=col + 1).value = data[row][col]
+        for row in data:
+            worksheet.append(row)
 workbook.remove(workbook.active)
 workbook.save('test.xlsx')
